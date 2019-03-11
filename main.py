a761be2262d616a18a7119a8c2654266087a7ed8
hornig <chefkueche@hornshouse.com>
2019-03-11 21:56:33 +0100

    added CRC 1bit fix based on lookup tables
    added
    crc 1bit fix. done with lookup tables. tables are created based on correct sample messages. works for 1 error in a message.
    
    made code a bit faster by adding continues. could be still faster though.
    
    finding more messages now, but still not all! let's find them! :)

--- main.py
+++ main.py
@@ -79,10 +79,159 @@ def crc(msg, encoding=False):
     rest = np2bin(msgnpbin[-24:])
     return rest
 
+
+def fix_msg_1bit(msg_bin, pos):
+
+    msg_list = list(msg_bin)
+
+    msg_bin_corrected = ""
+
+    parity_length = 24
+    for c in range(len(msg_bin) - parity_length):
+
+        # bit flipping
+        if msg_list[c] == "1":
+            msg_list[c] = "0"
+        else:
+            msg_list[c] = "1"
+
+
+        if int(crc(hex(int(''.join(msg_list), 2))[2:2 + 14]), 2) == 0:
+            #print(pos, "corrected at", c,  ''.join(msg_list))
+            msg_bin_corrected = ''.join(msg_list)
+            break
+
+        # bit back flipping
+        if msg_list[c] == "1":
+            msg_list[c] = "0"
+        else:
+            msg_list[c] = "1"
+
+    return msg_bin_corrected
+
+
+def flip_bit(msg_bin, position):
+    '''
+    flipping bits, the old way.
+    when the 1 bit error was detected before, the bit position is used here to flipt the bit accordingly.
+    the corrected binary message is returned
+
+    :param msg_bin:
+    :param position:
+    :return msg_bin_corrected:
+    '''
+
+    msg_bin_corrected = list(msg_bin)
+    if msg_bin_corrected[position[0][0]] == "0":
+        msg_bin_corrected[position[0][0]] = "1"
+    else:
+        msg_bin_corrected[position[0][0]] = "0"
+
+    msg_bin_corrected = ''.join(msg_bin_corrected)
+
+    return msg_bin_corrected
+
+
+def create_crc_lut_056bit():
+    '''
+    creating the lookup-table that we will need to find the position of a 1bit error in the 56bit long string.
+
+    calculating the reminder for the full sequence of bits that was transmitted perfectly.
+    then one bit is flipped and the reminder is calculated. it was checked, that the reminder, that is not zero anymore
+    is the same for any correctly transferred bit sequence.
+    this is repeated for each index position and the reminders are calculated and stored.
+
+    :aim: if the reminder of any message can be found in this lookup table,
+    then it is a 1 bit error,
+    and then the location is located.
+    the last step is to flip this one bit and it should be repaired.
+
+    :premise: the message should not be so bit scrambled, that the checksum is correct for the wrong messages.
+    should be unlikely.
+
+    :return: list of crc24 reminders for each 1bit error.
+    '''
+
+
+    msg_bin = "01011101010011001010010110110111101111110111010001101101"
+
+    crc_lut_056bit = []
+    msg_tmp = list(msg_bin)
+    for c in range(len(msg_tmp)):
+        # bit flipping
+        if msg_tmp[c] == "1":
+            msg_tmp[c] = "0"
+        else:
+            msg_tmp[c] = "1"
+
+        # print(c, ''.join(msg_tmp))
+        # print(c, hex(int(crc(hex(int(''.join(msg_tmp), 2))[2:]), 2)))
+        crc_lut_056bit.append(hex(int(crc(hex(int(''.join(msg_tmp), 2))[2:]), 2)))
+
+        # bit back flipping
+        if msg_tmp[c] == "1":
+            msg_tmp[c] = "0"
+        else:
+            msg_tmp[c] = "1"
+
+    return crc_lut_056bit
+
+
+def create_crc_lut_112bit():
+    '''
+        creating the lookup-table that we will need to find the position of a 1bit error in the 112bit long string.
+
+        calculating the reminder for the full sequence of bits that was transmitted perfectly.
+        then one bit is flipped and the reminder is calculated. it was checked, that the reminder, that is not zero anymore
+        is the same for any correctly transferred bit sequence.
+        this is repeated for each index position and the reminders are calculated and stored.
+
+        :aim: if the reminder of any message can be found in this lookup table,
+        then it is a 1 bit error,
+        and then the location is located.
+        the last step is to flip this one bit and it should be repaired.
+
+        :premise: the message should not be so bit scrambled, that the checksum is correct for the wrong messages.
+        should be unlikely.
+
+        :return: list of crc24 reminders for each 1bit error.
+        '''
+
+
+    msg_bin = "1000110100111100010010001001000110011001000000001001111010110101101000000000011100001100101000111111011000001010"
+
+    crc_lut_112bit = []
+    msg_tmp = list(msg_bin)
+    for c in range(len(msg_tmp)):
+        # bit flipping
+        if msg_tmp[c] == "1":
+            msg_tmp[c] = "0"
+        else:
+            msg_tmp[c] = "1"
+
+        # print(c, ''.join(msg_tmp))
+        # print(c, hex(int(crc(hex(int(''.join(msg_tmp), 2))[2:]), 2)))
+        crc_lut_112bit.append(hex(int(crc(hex(int(''.join(msg_tmp), 2))[2:]), 2)))
+
+        # bit back flipping
+        if msg_tmp[c] == "1":
+            msg_tmp[c] = "0"
+        else:
+            msg_tmp[c] = "1"
+
+    return crc_lut_112bit
+
+
+
 def main():
     print("Hello World!")
-
     time_start = time.time()
+
+    # creating the lookup tables that we will need for the 1bit fixes.
+    # both message length, 56 bit and 112 bit, each.
+    crc_lut_056bit = create_crc_lut_056bit()
+    crc_lut_112bit = create_crc_lut_112bit()
+
 
     # loading the iq data from your sdr receiver
     filename = "20190216_horn_1550327401.dat"
@@ -94,10 +243,10 @@ def main():
     # offset = 0 for rtlsdr iq rcordings in 8bit binar form
     # offset = 44 for sdrsharp iq recordings in 8bit binary form
 
+
     # rtlsdr is recording binary unsigned. so making it signed and just using the amplitude of the iq data
-    data = -127 + data
-    #data = np.subtract(data, -127)
-    samples = (data[0::2] ** 2 + data[1::2] ** 2) ** 0.5
+    data = -127 + data[:]
+    samples = np.sqrt(np.square(data[0::2]) + np.square(data[1::2]))
 
     # the adsb preamble is 1010000101000000
     # 1 =
@@ -134,7 +283,8 @@ def main():
             if len(msg) == 224:
                 for i in range(0, len(msg), 2):
 
-                    # manchaster coding
+                    # manchester coding
+                    # https://en.wikipedia.org/wiki/Manchester_code
                     if msg[i] > msg[i + 1]:
                         msg_bin += "1"
                     else:
@@ -143,12 +293,44 @@ def main():
 
                 # maybe there is a better way to distingish between short and extended squitter, but at least this works
                 if int(crc(hex(int(msg_bin, 2))[2:]), 2) == 0:
+                    #print(msg_bin)
                     print("112", count, hex(int(msg_bin, 2))[2:], s + msg_start, (s + msg_start) / len(samples))
                     count += 1
+                    continue
+
+                else:
+                    # finding where the 1 bit error most likely occured for a 112 bit message
+                    position = np.argwhere(np.array(crc_lut_112bit) == hex(int(crc(hex(int(msg_bin, 2))[2:]), 2)))
+
+                    if len(position) > 0:
+                        # correcting the msg here
+                        msg_bin_corrected = flip_bit(msg_bin, position)
+
+                        if int(crc(hex(int(msg_bin_corrected, 2))[2:]), 2) == 0:
+                            # print(msg_bin)
+                            print("112_c", count, hex(int(msg_bin_corrected, 2))[2:], s + msg_start, (s + msg_start) / len(samples))
+                            count += 1
+                            continue
+
 
                 if int(crc(hex(int(msg_bin, 2))[2:2 + 14]), 2) == 0:
                     print("056", count, hex(int(msg_bin, 2))[2:2 + 14], s + msg_start, (s + msg_start) / len(samples))
                     count += 1
+                    continue
+
+                else:
+                    # finding where the 1 bit error most likely occured for a 56 bit message
+                    position = np.argwhere(np.array(crc_lut_056bit)==hex(int(crc(hex(int(msg_bin, 2))[2:2 + 14]), 2)))
+
+                    if len(position) > 0:
+                        # correcting the msg here
+                        msg_bin_corrected = flip_bit(msg_bin, position)
+
+                        if int(crc(hex(int(msg_bin_corrected, 2))[2:2 + 14]), 2) == 0:
+                            # print(msg_bin)
+                            print("056_c", count, hex(int(msg_bin_corrected, 2))[2: 2 + 14], s + msg_start, (s + msg_start) / len(samples))
+                            count += 1
+                            continue
 
     print("total time", time.time() - time_start)
 
