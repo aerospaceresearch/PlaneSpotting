f4a728eed5e9030d789ba1fc95fb7b663d77c0db
Shoumik Dey <shoumikdey123@gmail.com>
2019-06-22 15:52:04 +0530

    Implementation of version 1 operation status msgs

--- planespotting/decoder.py
+++ planespotting/decoder.py
@@ -179,8 +179,25 @@ def decode(data):
             continue
         if identifier6(df, tc):
             decode_id = 6
-            frames['ICAO'] = getICAO(frames['adsb_msg'])
-            frames['ver'] = int(hexToDec(frames['adsb_msg'][8:22])[40:43], 2)
+            adsb_msg = frame['adsb_msg']
+            frames['ICAO'] = getICAO(adsb_msg)
+            adsb_msg_data = hexToDec(adsb_msg[8:22])
+            ver = int(adsb_msg_data[40:43], 2)
+
+            if ver == 1:
+                frames['stype_code'] = int(adsb_msg_data[5:8], 2)
+                frames['sccc'] = int(adsb_msg_data[8:20], 2)
+                frames['lw_codes'] = int(adsb_msg_data[20:24], 2)
+                frames['op_mc'] = int(adsb_msg_data[24:40], 2)
+                frames['ver'] = ver
+                frames['NIC'] = int(adsb_msg_data[43], 2)
+                frames['NACp'] = int(adsb_msg_data[44:48], 2)
+                frames['SIL'] = int(adsb_msg_data[50:53], 2)
+                frames['HRD'] = int(adsb_msg_data[53], 2)
+
+            
+
+
             exit(frames)
             continue
         if identifier7(df, tc):
