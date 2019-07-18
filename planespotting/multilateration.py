7252ee731a75cfb9dc9e338264aeb6e50011aae8
Shoumik Dey <shoumikdey123@gmail.com>
2019-07-19 00:54:33 +0530

    Debugging the overlapping function

--- planespotting/multilateration.py
+++ planespotting/multilateration.py
@@ -49,6 +49,7 @@ def check_file_overlap(file1, file2):
     data1_rec_end = data1['meta']['rec_end']
     data2_rec_start = data2['meta']['rec_start']
     data2_rec_end = data2['meta']['rec_end']
+    print(data1_rec_start, data1_rec_end, data2_rec_start, data2_rec_end)
     if (data1_rec_start <= data2_rec_start and data2_rec_end >= data1_rec_start) or (data2_rec_start <= data1_rec_start and data2_rec_end <= data1_rec_end):
         return True
     else:
